3e0ee674f9d96b7925cf0445f70b2f16f5909922
zudi-lin <linzudi@g.harvard.edu>
2019-04-30 22:32:58 -0400

    upload model

---
+++ script/upload_model.py
@@ -0,0 +1,32 @@
+'''Upload data and model weights to Amazon S3'''
+
+from system_utils import *
+import os, glob, sys
+import time
+import shutil
+import logging
+import boto3
+from botocore.exceptions import ClientError
+sys.path.append(os.path.abspath('../'))
+
+ACCESS_KEY = "AKIAYSSR3P6HBYS35VUA"
+SECRET_KEY = "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"
+REGION = "us-east-1"
+BUCKET = "2019.harvard.cs244r"
+
+client = boto3.client(
+    's3',
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY,
+    region_name=REGION
+)
+
+response = client.list_buckets()['Buckets']
+
+model_dir = '../outputs/checkpoint/'
+model_list = glob.glob(model_dir+'*.t7')
+print('number of models: ', len(model_list))
+for item in model_list:
+    path, filename = os.path.split(item)
+    print(item, path, filename)
+    upload_file(client, item, BUCKET, 'fedrated_models/', filename)
